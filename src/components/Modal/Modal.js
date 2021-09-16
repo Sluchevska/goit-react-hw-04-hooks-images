f83fc9701226f2818b9d44da144addddb0af0c65
Aleksanrda Sluchevska <alaska3a@gmail.com>
2021-09-16 17:18:39 +0100

    update

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -33,39 +33,6 @@ export default function Modal({ src, tags, onClose }) {
     );
 }
 
-// export default class Modal extends Component {
-//   componentDidMount() {
-//     window.addEventListener('keydown', this.handleKeyDown);
-//   }
-
-//   componentWillUnmount() {
-//     window.removeEventListener('keydown', this.handleKeyDown);
-//   }
-
-//   handleKeyDown = e => {
-//     if (e.code === 'Escape') {
-//       this.props.onClose();
-//     }
-//   };
-
-//   handleBackDrop = e => {
-//     if (e.currentTarget === e.target) {
-//       this.props.onClose();
-//     }
-//   };
-
-//   render() {
-//     const { src, alt } = this.props;
-//     return createPortal(
-//       <div className="Overlay" onClick={this.handleBackDrop}>
-//         <div className="Modal">
-//           <img src={src} alt={alt} />
-//         </div>
-//       </div>,
-//       modalRoot,
-//     );
-//   }
-// }
 
 Modal.propTypes = {
   src: PropTypes.string.isRequired,
